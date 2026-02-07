260ae2e6fda35d35e7c6c989237ba2bff98211f6
Nova <aelithron@gmail.com>
2026-02-07 14:42:59 -0700

    add eslint, better config and setup stuff, and a test thingy

--- index.ts
+++ index.ts
@@ -5,15 +5,28 @@ let app: App;
 async function startApp() {
   dotenv.config({ quiet: true });
   if (!process.env.SLACK_BOT_TOKEN) throw new Error('"SLACK_BOT_TOKEN" environment variable is missing!');
-  if (process.env.SOCKET_MODE) {
-    if (!process.env.SLACK_APP_TOKEN) throw new Error('"SLACK_APP_TOKEN" environment variable is missing, and Socket Mode is on!');
+  if (process.env.SOCKET_MODE === "true") {
+    if (!process.env.SLACK_APP_TOKEN) throw new Error('"SLACK_APP_TOKEN" environment variable is missing (and Socket Mode is on)!');
     app = new App({
       token: process.env.SLACK_BOT_TOKEN,
       socketMode: true,
       appToken: process.env.SLACK_APP_TOKEN,
     });
+  } else {
+    if (!process.env.SLACK_SIGNING_SECRET) throw new Error('"SLACK_SIGNING_SECRET" environment variable is missing (and Socket Mode is off)!');
+    app = new App({
+      token: process.env.SLACK_BOT_TOKEN,
+      socketMode: false,
+      signingSecret: process.env.SLACK_SIGNING_SECRET
+    });
   }
   await app.start();
-  app.logger.info("⚡️ Bolt app is running!");
+  app.logger.setName("novabot-slack");
+  const selfInfo = await app.client.auth.test();
+  app.logger.info(`is ready as ${selfInfo.user} (${selfInfo.user_id}) :D`);
+  app.message("NovaBot Testing", async ({ message, say }) => {
+    // @ts-expect-error - message.thread_ts does exist
+    say({ text: "test working :3", thread_ts: message.thread_ts || message.ts });
+  });
 }
 startApp();
