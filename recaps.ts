1f99a4bd295a71a2845802229fafb4e4add2fda9
Nova <aelithron@gmail.com>
2026-02-09 20:17:00 -0700

    fix some block kit issues with universal status integration

--- recaps.ts
+++ recaps.ts
@@ -6,12 +6,12 @@ export default async function dailyRecap(app: App) {
 	try {
 		status = await getUniversalStatus("aelithron@gmail.com");
 	} catch (e) {
-		app.logger.error("Couldn't get the status from Universal Status!", e);
+		app.logger.error(`Couldn't get the status from Universal Status!\n${e}`);
 	}
 	const publicRecap = await app.client.chat.postMessage({
 		channel: "C0ADSBWENAY", text: "hi <@U08RJ1PEM7X>, daily recap time! how was your day? :3", blocks: [
 			{ type: "section", text: { type: "mrkdwn", text: "hi <@U08RJ1PEM7X>, daily recap time! how was your day? :3" } },
-			{ type: "section", text: { type: "mrkdwn", text: `${status ? `nova's current status: ${status.emoji} ${status.status}` : "" }` } },
+			{ type: "section", text: { type: "mrkdwn", text: `nova's current status: ${status ? `*${status.emoji} ${status.status}*` : `:x: error getting status!` }` } },
 			{ type: "actions", elements: [{ type: "button", text: { type: "plain_text", text: ":crescent_moon: done!", emoji: true }, action_id: "public_daily_recap" }] }
 		]
 	});
@@ -22,19 +22,22 @@ export default async function dailyRecap(app: App) {
 	await app.client.reactions.add({ channel: publicRecap.channel!, timestamp: publicRecap.ts!, name: "zzz" });
 }
 export async function privateRecap(app: App, permaLink?: string) {
-	let status;
+	let status = null;
 	try {
 		status = await getUniversalStatus("aelithron@gmail.com");
 	} catch (e) {
-		app.logger.error("Couldn't get the status from Universal Status!", e);
+		app.logger.error(`Couldn't get the status from Universal Status!\n${e}`);
 	}
 	const privateRecap = await app.client.chat.postMessage({
 		channel: "C0ADRH7KXN1", text: `<@U08RJ1PEM7X>, private recap time! :3\n${permaLink ? `while you wait, you can look at <${permaLink}|the public recap>!` : ""}`, blocks: [
 			{ type: "section", text: { type: "mrkdwn", text: `<@U08RJ1PEM7X>, private recap time! :3\n${permaLink ? `while you wait, you can look at <${permaLink}|the public recap>!` : ""}` } },
-			{ type: "section", text: { type: "mrkdwn", text: `${status ? `nova's current status: ${status.emoji} ${status.status}` : "" }` } },
+			{ type: "section", text: { type: "mrkdwn", text: `nova's current status: ${status ? `*${status.emoji} ${status.status}*` : `:x: error getting status!` }` } },
 			{ type: "actions", elements: [{ type: "button", text: { type: "plain_text", text: ":crescent_moon: done!", emoji: true }, value: permaLink || "", action_id: "private_daily_recap" }] }
 		]
 	});
-	if (!privateRecap.ok) throw new Error(`issue with sending a private recap!\n${privateRecap}`);
+	if (!privateRecap.ok) {
+		app.logger.error(`issue with sending a private recap!\n${privateRecap}`);
+		return;
+	}
 	await app.client.reactions.add({ channel: privateRecap.channel!, timestamp: privateRecap.ts!, name: "zzz" });
 }
--- utils/remote.ts
+++ utils/remote.ts
@@ -1,7 +1,6 @@
-export async function getUniversalStatus(email: string): Promise<{ status: string, emoji: string, expiry: Date | null, setAt: Date } | null> {
+export async function getUniversalStatus(email: string): Promise<{ status: string, emoji: string, expiry: Date | null, setAt: Date }> {
   const res = await fetch(`https://status.novatea.dev/api/status?email=${email}`);
   const body = await res.json() as { status: string, emoji: string, expiry?: string, setAt: string } | { error: string, message: string };
   if ("error" in body) throw new Error(`${body.message} (${body.error})`);
-  if (body.status === "No status set.") return null;
   return { status: body.status, emoji: body.emoji, expiry: (body.expiry ? new Date(body.expiry) : null), setAt: new Date(body.setAt) };
 }
